908e026990feb27dcf16d366eb050d2622587a29
勝崴 施 <4b1g0178@office.stust.edu.tw>
2023-03-18 07:43:36 +0800

    加入專案檔案。

---
+++ ITSA_8/ITSA_8.cpp
@@ -0,0 +1,32 @@
+﻿// ITSA_8.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,i,ans=0;
+    cin >> x;
+
+    for (i = 2;i < x; i++) 
+    {
+
+        if (x % i == 0)
+            ans += 1;
+            }
+    if (ans == 0) {
+
+        cout << "YES" << endl;
+    }
+
+    else {
+
+        cout << "NO" << endl;
+
+    }
+
+    
+}
+
+
+
